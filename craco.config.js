58619bb93d9074c167c7c690f7ea721f834e8083
LiShuxue <1149926505@qq.com>
2021-09-30 19:13:00 +0800

    添加各种基础架子

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    devServer: {
-        port: 3002,
-        proxy: {
-            '/api': {
-                target: process.env.REACT_APP_API_URL,
-                pathRewrite: { '^/api': '' },
-                secure: false,
-                changOrigin: true
-            }
-        }
+  devServer: {
+    port: 3002,
+    proxy: {
+      '/api': {
+        target: process.env.REACT_APP_API_URL,
+        pathRewrite: { '^/api': '' },
+        secure: false,
+        changOrigin: true,
+      },
     },
-}
+  },
+};
