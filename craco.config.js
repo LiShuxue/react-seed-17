9c3785526b70be6b5cbb02e723b1cc0d137d1351
LiShuxue <1149926505@qq.com>
2021-10-09 12:07:02 +0800

    craco config的bug

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,11 @@
+// craco.config.js中目前有个bug，除了process.env.NODE_ENV，不能使用一些其他的自定义变量，如，process.env.REACT_APP_API_URL。
+
 module.exports = {
   devServer: {
     port: 3002,
     proxy: {
       '/api': {
-        target: process.env.REACT_APP_API_URL,
+        target: 'http://localhost:4000',
         pathRewrite: { '^/api': '' },
         secure: false,
         changOrigin: true,
